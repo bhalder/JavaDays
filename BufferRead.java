77be2f05164ae9cb1d38785d48a00beb3a1c1589
Barun Halder <bhalder@bhalder@andrew.cmu.edu>
2016-02-13 13:13:51 -0500

    BufferedRead Fixed

--- BufferRead.java
+++ BufferRead.java
@@ -1,14 +1,19 @@
-import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.IOException;
 
 
 public class BufferRead {
 
     public static void main( String[] args ) {
-        BufferedReader bf = new BufferedReader( new FileReader(System.in ) );
-        String name = bf.readLine();
+        BufferedReader bf = new BufferedReader( new InputStreamReader(System.in ) );
 
-        System.out.println("Hello! "+name);
-        
+        // You must ALWAYS catch the input IOException
+        try {
+            String name = bf.readLine();
+            System.out.println("Hello! "+name);
+        } catch (IOException e ) {
+            System.out.println("IOException");
+        }        
     }
 }
